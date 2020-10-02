a2677fddc3df92e56563101e7e2cb23622c7d3fd
James Collins <jimwcollins@gmail.com>
2020-10-02 10:01:37 +0100

    sets up repo and creats song.js

---
+++ song.js
@@ -0,0 +1,16 @@
+/*
+    Released	March 22, 1990
+    Recorded	1989
+    Genre	
+    Industrial rockelectronic rockindustrial dance
+    Length	5:00
+    Label	TVT
+    Songwriter(s)	Trent Reznor
+    Producer(s)	
+    Trent ReznorFloodAdrian SherwoodKeith LeBlanc
+*/
+
+const mySong = {
+
+}
+
