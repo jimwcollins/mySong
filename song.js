bc3b570b4d28b2df7a53b8c638db76992b2ae768
James Collins <jimwcollins@gmail.com>
2020-10-02 10:38:43 +0100

    solves song object challenge

--- song.js
+++ song.js
@@ -1,16 +1,47 @@
-/*
-    Released	March 22, 1990
-    Recorded	1989
-    Genre	
-    Industrial rockelectronic rockindustrial dance
-    Length	5:00
-    Label	TVT
-    Songwriter(s)	Trent Reznor
-    Producer(s)	
-    Trent ReznorFloodAdrian SherwoodKeith LeBlanc
-*/
-
 const mySong = {
-
+    title: "Head Like a Hole",
+    artist: "Nine Inch Nails",
+    songwriter: "Trent Reznor",
+    genre: ['Industrial', 'rock', 'electronic'],
+    released: 1989,
 }
 
+const mySong2 = {
+    title: "Livin' On A Prayer",
+    artist: "Bon Jovi",
+    songwriter: ['Jon Bon Jovi', 'Richie Sambora', 'Desmond Child'],
+    genre: ['Soft rock'],
+    released: 1986,
+}
+
+function displaySong(objSong) {
+    for (keyValue of Object.entries(objSong)) {
+        const key = keyValue[0];
+        const value = keyValue[1];
+    
+        // Handle arrays separately
+        if (Array.isArray(value)) {
+            // Check the length of the array and display key accordingly
+            let genreString = value.length > 1 ? `${key}s: ` : `${key}: `;
+            
+            // Display value in array depending on position
+            for (let i = 0; i < value.length; i++) {
+                if (i === 0) {
+                    genreString += `${value[i]}`;
+                } else if (i !== value.length - 1) {
+                    genreString += `, ${value[i]}`;
+                } else {
+                    genreString += ` and ${value[i]}`;
+                }
+            }
+            console.log(genreString);
+        } else {
+            console.log(`${keyValue[0]}: ${keyValue[1]}`);
+        }
+    }
+    console.log('\n');
+}
+
+console.log('These are my favourite songs:\n');
+displaySong(mySong);
+displaySong(mySong2);
